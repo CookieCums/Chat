23f3b1ed8cca091ea9d52c63c8027c963e054b64
Gautam <159130133+CookieCums@users.noreply.github.com>
2025-03-07 09:29:51 +0530

    Update server.py

--- server.py
+++ server.py
@@ -48,4 +48,4 @@ def get_updates():
         time.sleep(0.5)  # sleep briefly to reduce CPU load
 
 if __name__ == '__main__':
-    app.run(threaded=True, debug=True, host='0.0.0.0', port=5000)
+    app.run(threaded=True, debug=True, host='0.0.0.0', port=10000)
